9aef409a5f4cc31186beb8cd3e779985f52d4af5
arthur <arthur+git@m13g.com>
2024-12-04 09:20:37 +0100

    added a normalized plot

--- multi_visualizer.py
+++ multi_visualizer.py
@@ -10,7 +10,9 @@ gender_column = 2
 
 
 # show the general layout and the kind of plots we can use for fitting
+# here we split by the category held in the category_column
 _, sub_datasets = dv.split_on_column(headers, dataset, category_column)
+# now we plot all these seperately (so with different scales!)
 big_fig, figs = plt.subplots(4, 4)
 for x in range(4):
     for y in range(4):
@@ -26,8 +28,25 @@ big_fig.align_labels()
 plt.tight_layout()
 plt.show()
 
+# now with the same scale
+big_fig, figs = plt.subplots(4, 4)
+for x in range(4):
+    for y in range(4):
+        index = x+4*y
+        header, data = dv.get_column(
+            headers, sub_datasets[index], months_column)
+        hist, bins = np.histogram(data, bins=np.arange(14))
+        hist = hist/np.mean(hist)
+        figs[x, y].plot(bins[1:-1:], hist[1:])
+        figs[x, y].set_title(sub_datasets[index][0][category_column])
+        figs[x, y].set_xlabel("month")
+        figs[x, y].set_ylabel("number of purchases")
+big_fig.align_labels()
+plt.tight_layout()
+plt.show()
 
-# show the assumption of one year being the same as the next
+
+# show the assumption of one year being the same as the next when it seems to be true
 category_to_show = 13
 header_column, data_column = dv.get_column(headers,
                                            sub_datasets[category_to_show],
@@ -42,7 +61,7 @@ plt.xlabel("month")
 plt.ylabel("number of purchases")
 plt.show()
 
-# show where this assumption of one year being the same as the next fails
+# show where this assumption of one year being the same as the next when it seems to be false
 category_to_show = 10
 header_column, data_column = dv.get_column(headers,
                                            sub_datasets[category_to_show],
