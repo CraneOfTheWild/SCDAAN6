d7209368b788c78c79e64d9ab7966ae90e9e59fb
arthur <arthur+git@m13g.com>
2024-12-04 08:52:02 +0100

    now also show the gender

--- multi_visualizer.py
+++ multi_visualizer.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 headers, dataset = dv.read_csv("data/file.csv")
 months_column = 18
 category_column = 9
-_, sub_datasets = dv.split_on_column(headers, dataset, category_column)
+gender_column = 2
 
-# # show the general layout and the kind of plots we can use
+
+# show the general layout and the kind of plots we can use for fitting
+_, sub_datasets = dv.split_on_column(headers, dataset, category_column)
 big_fig, figs = plt.subplots(4, 4)
 for x in range(4):
     for y in range(4):
@@ -21,7 +23,9 @@ for x in range(4):
         figs[x, y].set_xlabel("month")
         figs[x, y].set_ylabel("number of purchases")
 big_fig.align_labels()
+plt.tight_layout()
 plt.show()
+
 
 # show the assumption of one year being the same as the next
 category_to_show = 13
@@ -36,4 +40,36 @@ plt.plot(bins[1:-1], hist[1:])
 plt.title(sub_datasets[category_to_show][0][category_column])
 plt.xlabel("month")
 plt.ylabel("number of purchases")
+plt.show()
+
+# show where this assumption of one year being the same as the next fails
+category_to_show = 10
+header_column, data_column = dv.get_column(headers,
+                                           sub_datasets[category_to_show],
+                                           months_column)
+multi_year_sub_data = data_column+12
+multi_year_sub_data = np.hstack((multi_year_sub_data,
+                                 data_column))
+hist, bins = np.histogram(multi_year_sub_data, bins=np.arange(26))
+plt.plot(bins[1:-1], hist[1:])
+plt.title(sub_datasets[category_to_show][0][category_column])
+plt.xlabel("month")
+plt.ylabel("number of purchases")
+plt.show()
+
+
+# now with gender
+_, sub_datasets = dv.split_on_column(headers, dataset, gender_column)
+big_fig, figs = plt.subplots(2)
+for x in range(2):
+    index = x
+    header, data = dv.get_column(
+        headers, sub_datasets[index], months_column)
+    hist, bins = np.histogram(data, bins=np.arange(14))
+    figs[x].plot(bins[1:-1:], hist[1:])
+    figs[x].set_title(sub_datasets[index][0][gender_column])
+    figs[x].set_xlabel("month")
+    figs[x].set_ylabel("number of purchases")
+big_fig.align_labels()
+plt.tight_layout()
 plt.show()
