e46e9428f307595c4d491adc0f42d3acf22f48fd
arthur <arthur+git@m13g.com>
2024-12-11 07:40:59 +0100

    fourier fit using a rolling average with setable window size

--- fourier_fit.py
+++ fourier_fit.py
@@ -6,10 +6,10 @@ import pandas as pd
 FILENAME = "data/file.csv"
 DATE_COLUMN_NAME = "Transaction_Date"
 CATEGORY_COLUMN_NAME = "Product_Category"
-AVG_OVER_DAYS = 30
+FOURIER_COMPONENTS = 30
 
 
-def fourier_fit(nr_days):
+def rolling_average(nr_days):
     # read in the data
     df = pd.read_csv(FILENAME, parse_dates=[DATE_COLUMN_NAME])
     # aggregate by date
@@ -18,14 +18,64 @@ def fourier_fit(nr_days):
     # sort for certainty
     df_per_day = df_per_day.sort_index()
     # get roling average
-    df_rolling_avg = df_per_day.rolling(nr_days).mean()
+    df_rolling_avg = df_per_day.rolling(window=nr_days,
+                                        center=True,
+                                        min_periods=1).mean()
+    df_rolling_avg = df_rolling_avg.dropna()
     # plotting
     df_rolling_avg.plot.line(ax=plt.gca())
+    plt.xlabel("Date")
     plt.ylabel("Sales_Number")
+    plt.title(f"roling average of sales for surrounding {nr_days} days")
     plt.show()
 
 
-# this uses np.fft
+def fourier_fit(nr_days=1):
+    # read in the data
+    df = pd.read_csv(FILENAME, parse_dates=[DATE_COLUMN_NAME])
+    # aggregate by date
+    df_per_day = pd.crosstab(df[DATE_COLUMN_NAME].dt.date,
+                             df[CATEGORY_COLUMN_NAME])
+    # sort for certainty
+    df_per_day = df_per_day.sort_index()
+    # get roling average
+    df_rolling_avg = df_per_day.rolling(window=nr_days,
+                                        center=True,
+                                        min_periods=1).mean()
+    df_rolling_avg = df_rolling_avg.dropna()
+    # convert to numpy for use of np.fft
+    column_names = df_rolling_avg.columns.tolist()
+    data = df_rolling_avg.to_numpy()
+
+    big_fig, figs = plt.subplots(4, 4)
+    # save the fourier analysis values
+    fourier_values = []
+    for x in range(4):
+        for y in range(4):
+            index = x + 4 * y
+            # make a fourier fit
+            fft_analysis = np.fft.fft(data.T[index])
+            fft_analysis_truncated = np.zeros_like(fft_analysis)
+            fft_analysis_truncated[:FOURIER_COMPONENTS] = fft_analysis[:FOURIER_COMPONENTS]
+            fourier_values.append(fft_analysis[:FOURIER_COMPONENTS])
+            reconstructed_data = np.fft.ifft(fft_analysis_truncated).real
+            figs[x, y].plot(df_rolling_avg.index, data.T[index],
+                            label="Original Rolling Average")
+            figs[x, y].plot(df_rolling_avg.index, reconstructed_data,
+                            label='Fourier Fit', linestyle='--')
+            figs[x, y].set_xlabel("Date")
+            figs[x, y].set_ylabel("Sales Number")
+            figs[x, y].set_title(column_names[index])
+    big_fig.align_labels
+    plt.tight_layout()
+    big_fig.subplots_adjust(top=0.88, wspace=0.4, hspace=0.6)
+    plt.legend()
+    big_fig.suptitle(f"fourier analysis using {FOURIER_COMPONENTS} components on the rolling average of the {
+        nr_days} days surrounding a date")
+    plt.show()
+    return fourier_values
+
+
 def months_fit():
     # read in the data
     headers, dataset = dv.read_csv("data/file.csv")
@@ -73,4 +123,4 @@ def months_fit():
     plt.show()
 
 
-fourier_fit(30)
+fourier_fit(14)
